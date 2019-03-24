10ab978262527ab62c0977f4803263470bc7b50f
unknown <AlexanderSchlechter@aschlechter.telonic.de>
2019-03-24 18:53:27 +0100

    GPS Watch Postition

--- testing.js
+++ testing.js
@@ -15,10 +15,26 @@ window.addEventListener('devicemotion', function(event) {
 
 });
 
-navigator.geolocation.getCurrentPosition(function(position) {
+// navigator.geolocation.getCurrentPosition(function(position) {
+
+// });
+
+function geo_success(position) {
 document.getElementById("gps").innerHTML = "<h1>" + "latitude: " + position.coords.latitude + "<br>" 
 + "longitude: " + position.coords.longitude + "</h1>";
-});
+}
+
+function geo_error() {
+  alert("Entschuldigung, keine Positionsinformationen sind verfügbar.");
+}
+
+var geo_options = {
+  enableHighAccuracy: true, 
+  maximumAge        : 30000, 
+  timeout           : 27000
+};
+
+var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
 
 }
 );
